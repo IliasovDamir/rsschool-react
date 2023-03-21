b1bd0240b3648d182eadac44b7248cd76af35ec8
IliasovDamir <ilyasovdh@gmail.com>
2023-03-21 21:39:24 +0300

    fix: ErrorPage test and add link to go home

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './ErrorPage.css';
 
 class ErrorPage extends Component {
@@ -7,8 +8,9 @@ class ErrorPage extends Component {
       <section className="section_about">
         <h1>Not Found, Error 404</h1>
         <p>
-          The page you are looking for no longer exists. Perhaps you can return back to the site’s
-          homepage and see if you can find what you are looking for.
+          The page you are looking for no longer exists. Perhaps you can{' '}
+          <Link to="/">return back</Link> to the site’s homepage and see if you can find what you
+          are looking for.
         </p>
       </section>
     );
